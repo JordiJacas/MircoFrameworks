76c364e35408c8797cf44102ea6515fb93c26d6b
JordiJacas <jjacasventura@iesesteveterradas.cat>
2018-01-12 20:18:38 +0100

    Acabado

--- world.py
+++ world.py
@@ -1,20 +1,23 @@
 from bottle import route, run, template, view, get, post, request
 from random import randint
 @get('/hello/world')
-
 def hello():
 	numAlt = randint(1,100)
-	num = None
+	num = " "
 	return template('world_template',num=num, numAlt = numAlt)
 
 @post('/hello/world')
-
 def hola():
 	num = request.forms.get('fname')
 	numAlt = request.forms.get('numAlt')
 
 	if numAlt == num:
-		return template('<h1>Hola</h1>')
+		return template('world_template',num="Has Ganado!!! El numero es " + numAlt, numAlt = numAlt)
+	elif numAlt < num:
+		return template('world_template',num="El numero es menor", numAlt = numAlt)
+	elif numAlt > num:
+		return template('world_template',num="El numero es mayor", numAlt = numAlt)
+
 run(host='localhost', port=8080)
 
 
--- world_template.tpl
+++ world_template.tpl
@@ -6,12 +6,13 @@
 <body>
 	<h2>Adivina el numero</h2>
 
-	<p>Numero: {{num}}<p>
-	<p>Numero: {{numAlt}}<p>
-	<form action='/hello/world' method="post">
+	<p>{{num}}<p>
+	<p>Numero a adivinar: {{numAlt}}<p>
+	<form action='/hello/world' method='post'>
 		Introdueix un numero: <input type="text" name="fname">
 		<input type="text" name="numAlt" value='{{numAlt}}' style='display:none'>
 		<input type="submit" value="Enviar">
+		<input type="submit" value="Vuelve a Empezar">
 	</form>
 </body>
 </html>
